f29b2d4e0889a3982034d518953a923530bbc4ae
justinaray828 <justinaray828@gmail.com>
2019-01-22 21:42:53 -0500

    Commiting ignore files and a few potentially useful scripts

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Attach script to Parent UI Canvas and child a menu to access. The script contains useful methods
+/// for the menu to access and use.
+/// </summary>
+public class MenuUI : MonoBehaviour
+{ 
+    [Header("Add Child Menu to toggle")]
+    [SerializeField] private GameObject menu;
+
+    private bool menuToggleBool = false;
+    private string cancelInputString = "Cancel"; //Unity input manager axis
+
+    void Update()
+    {
+        if( Input.GetButtonDown(cancelInputString) )
+        {
+            menu.SetActive(menuToggleBool);
+            menuToggleBool = !menuToggleBool;
+        }
+    }
+
+}
