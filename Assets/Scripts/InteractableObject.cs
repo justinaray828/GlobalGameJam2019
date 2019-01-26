51f80d41f936ccdfaca67380ce7a55c2dae3dbae
justinaray <justinaray828@gmail.com>
2019-01-26 12:46:52 -0500

    Created GameChangeInformation GameObject that will swap between the different games. Interactable object is able to change to specified game. Input controller uses click input to that will reference InteractableGameObject.

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -4,20 +4,63 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private InteractableObjectData interactableObjectData;
+    [SerializeField] private Canvas SpeechCanvas;
+    private GameChangeInformation gameChangeInformation;
+    private const string GAMECHANGEINFORMATIONTAG = "GameChangeInformation";
+
+    private void Start()
     {
-        
+        gameChangeInformation = GameObject.FindGameObjectWithTag(GAMECHANGEINFORMATIONTAG).GetComponent<GameChangeInformation>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>
+    /// Interact with clicked GameObject
+    /// </summary>
     public void Interact()
     {
+        switch(interactableObjectData.interactableObjectType)
+        {
+            case InteractableObjectData.InteractableObjectType.Text:
+                TextInteraction();
+                break;
+            case InteractableObjectData.InteractableObjectType.Image:
+                ImageInteraction();
+                break;
+            case InteractableObjectData.InteractableObjectType.MiniGame:
+                MiniGameInteraction();
+                break;
+        }
+    }
 
+    private void TextInteraction()
+    {
+
+    }
+
+    private void ImageInteraction()
+    {
+
+    }
+
+    private void MiniGameInteraction()
+    {
+        Debug.Log("Game Change to: " + interactableObjectData.miniGameType.ToString());
+
+        switch (interactableObjectData.miniGameType)
+        {
+            case InteractableObjectData.MiniGameType.Dancing:
+                gameChangeInformation.ChangeToDancingGame();
+                break;
+            case InteractableObjectData.MiniGameType.Fridge:
+                gameChangeInformation.ChangeToFridgeGame();
+                break;
+            case InteractableObjectData.MiniGameType.Flowers:
+                gameChangeInformation.ChangeToFlowerGame();
+                break;
+            default:
+                Debug.LogError("Mini game was activated but no mini game was chosen");
+                break;
+        }
     }
 }
