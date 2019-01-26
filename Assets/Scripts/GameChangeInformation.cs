60138f716a4b713c09ada8c6e3c31f324eb47de7
justinaray <justinaray828@gmail.com>
2019-01-26 10:06:06 -0500

    Added old man cube Added a scritable object for interacting with objects Added click interaction for input controller Added a script to hold the data for "scene" changes

---
+++ Assets/Scripts/GameChangeInformation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameChangeInformation : ScriptableObject
+{
+    public GameObject MainGame;
+    public GameObject DancingGame;
+    public GameObject FridgeGame;
+    public GameObject FlowerGame;
+
+    public GameObject MainGameController;
+    public GameObject DancingGameController;
+    public GameObject FridgeGameController;
+    public GameObject FlowerGameController;
+
+    public Camera MainGameCamera;
+    public Camera DancingGameCamera;
+    public Camera FridgeGameCamera;
+    public Camera FlowerGameCamera;
+
+    public void ChangeToDancingGame()
+    {
+        MainGame.SetActive(false);
+        MainGameController.SetActive(false);
+
+        DancingGame.SetActive(true);
+        DancingGameController.SetActive(true);
+    }
+}
