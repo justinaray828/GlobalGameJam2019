ad9dc0facdaf30ad600977a67647a4d5867f4e0e
Julie <jcpersin@ncsu.edu>
2019-01-27 12:00:47 -0500

    Example of success text

--- Assets/Scripts/GameChangeInformation.cs
+++ Assets/Scripts/GameChangeInformation.cs
@@ -46,6 +46,11 @@ public class GameChangeInformation : MonoBehaviour
         DancingGame.SetActive(false);
         MainGame.SetActive(true);
 
+        SpeechBubbleSettings settings = new SpeechBubbleSettings();
+        settings.MaxWidth = 300;
+        settings.TimeBetweenChars = 0.03f;
+        settings.TimeUntilClose = 3;
+
         if (pass)
         {
             if (puzzlename == "dance") 
@@ -53,6 +58,9 @@ public class GameChangeInformation : MonoBehaviour
                 if (danceSolved == false)
                 {
                     FindObjectOfType<AudioManager>().ToHomeMusicOnSuccess();
+                    settings.Text = "That was our wedding dance. It took me forever to learn it.";
+                    SpeechBubbleSettings[] settingsArray = {settings};
+                    SpeechBubble.Instance.DisplaySpeech(settingsArray);
                 }
                 danceSolved = true;
                 }
