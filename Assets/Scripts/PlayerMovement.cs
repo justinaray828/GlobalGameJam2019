dfa181f260102a91d7d238ece82c309c0972fb59
Bryan Kisby <bryan.kisby@gmail.com>
2019-01-26 22:59:31 -0500

    Changed main scene to start combining games Prefabed everything Fixed camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,80 +8,21 @@ public class PlayerMovement : MonoBehaviour
 
     public float Speed;
 
+    public Transform Model;
+
     private float _DesiredZDepth = -5f;
     private float _CurrentZDepth = -5f;
     private bool _StartedChangingDepth = false;
 
     private const string INTERATABLEOBJECTTAG = "InteractableObject";
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
-
-        bool changingDepth = false;
-
-        if (_StartedChangingDepth == false)
-        {
-            bool allowHorizontalMove = true;
-            
-            if (vertical != 0)
-            {
-                _StartedChangingDepth = true;
-                allowHorizontalMove = false;
-                
-                if (vertical > 0f)
-                {
-                    _DesiredZDepth = -5f;
-                }
-                else if (vertical < 0f)
-                {
-                    _DesiredZDepth = -15f;
-                }
-                
-                Vector3 depthChangeDirection = new Vector3(transform.position.x, transform.position.y, _DesiredZDepth);
-                depthChangeDirection -= transform.position;
-                int layerToIgnore = 1 << 8;
-                layerToIgnore = ~layerToIgnore;
-                
-                bool rayCastHit = Physics.Raycast(transform.position, depthChangeDirection.normalized, depthChangeDirection.magnitude);
-
-                if (_CurrentZDepth == _DesiredZDepth ||
-                    rayCastHit == true)
-                {
-                    _DesiredZDepth = _CurrentZDepth;              
-                    _StartedChangingDepth = false;
-                    allowHorizontalMove = true;
-                }
-            }
-            
-            if (allowHorizontalMove == true)
-            {
-                Controller.SimpleMove(new Vector3(horizontal * Speed, 0f, 0f));
-            }
-        }
-        else
-        {
-            float zDirection = _DesiredZDepth - _CurrentZDepth;
-            zDirection /= Mathf.Abs(zDirection);
-            
-            bool didWork = Controller.SimpleMove(new Vector3(0f, 0f, zDirection * Speed));
-            _CurrentZDepth = transform.position.z;
-
-            if (Mathf.Abs(_CurrentZDepth - _DesiredZDepth) <= 0.1f ||
-                didWork == false)
-            {
-                _CurrentZDepth = _DesiredZDepth;
-                _StartedChangingDepth = false;
-            }
-        }
+        
+        Controller.SimpleMove(new Vector3(horizontal * Speed, 0f, vertical * Speed));
 
         ClickInteraction();
     }
@@ -95,7 +36,7 @@ public class PlayerMovement : MonoBehaviour
         {
             Ray mousRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit mouseHit;
-            Physics.Raycast(mousRay, out mouseHit);
+            Physics.Raycast(mousRay, out mouseHit, 1 << 9);
 
             if (mouseHit.collider != null)
             {
