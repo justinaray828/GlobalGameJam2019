5850fb360e2a284852d8a88015553841357258a5
justinaray <justinaray828@gmail.com>
2019-01-27 02:41:59 -0500

    Flower Power

---
+++ Assets/FlowerHandController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerHandController : MonoBehaviour
+{
+    [SerializeField] private GameObject hand;
+    private GameObject grabbedFlower;
+    [SerializeField] private Sprite grabHand;
+    GameChangeInformation gameChangeInformation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hand.transform.position = GetWorldPositionOnPlane(Input.mousePosition, 0);
+        
+
+        if(grabbedFlower)
+        {
+            grabbedFlower.transform.position = hand.transform.position;
+        }
+        else
+        {
+            ClickInteraction();
+        }
+
+    }
+
+    private void ClickInteraction()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D mouseHit;
+            mouseHit = Physics2D.Raycast(mouseRay.origin, mouseRay.direction);
+
+            if (mouseHit.collider != null)
+            {
+                if (mouseHit.transform.tag == "WhiteRose" || mouseHit.transform.tag == "PurpleRose" || mouseHit.transform.tag == "RedRose")
+                {
+                    grabbedFlower = mouseHit.transform.gameObject;
+                    hand.GetComponent<SpriteRenderer>().sortingOrder = 1;
+                    hand.GetComponent<SpriteRenderer>().sprite = grabHand;
+                }
+            }
+        }
+    }
+
+    public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        xy.Raycast(ray, out distance);
+        return ray.GetPoint(distance);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "PurpleRose")
+        {
+            gameChangeInformation.ChangeToMainGame(true);
+        }
+        else
+        {
+            gameChangeInformation.ChangeToMainGame(false);
+        }
+    }
+
+}
